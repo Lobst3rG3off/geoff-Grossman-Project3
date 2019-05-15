bf837ef558bf6e28ba4ccc6f4b1d760053af4513
Lobst3rG3off <erip.pasei@gmail.com>
2019-05-15 16:36:56 -0400

    duct taping together JS to a form

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
+// ok steps to accomplish the JS part of this project.
+// create switch statement for sorting
+// set user form inputs as variables to save the information to push through switch statement
+// on submit the users values are printed out on the DOM with the dog being categorized within the output  
+
+
+
+// switch statement in non doc ready
+
+
+// value 
+const dogCategory = $('input[value=""]');
+
+switch (dogCategory) {
+    case 1:
+        console.log("Your dog is a Pupperino!");
+        break;
+    case 2:
+        console.log("Your dog is a Bean!");
+        break;
+    case 3:
+        console.log("Your dog is a Puppo!");
+        break;
+    case 4:
+        console.log("Your dog is a Pupper!");
+        break;
+    case 5:
+        console.log("Your dog is a Woofer!");
+        break;
+    case 6:
+        console.log("Your dog is a Boofer!");
+        break;
+    case 7:
+        console.log("Your dog is a Big Boofer!");
+        break;
+    default:
+        console.log("Ruh Roh! Try Again!");
+}
+
+
 $(function () {
     $('form').on('submit', function () {
         console.log('woof woof!')
