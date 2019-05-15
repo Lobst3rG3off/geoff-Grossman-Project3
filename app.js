ac92f3448ab0a9c9145df4e5cc27628109df481d
Lobst3rG3off <erip.pasei@gmail.com>
2019-05-15 17:32:40 -0400

    fixed functionality

--- app.js
+++ app.js
@@ -8,38 +8,51 @@
 // switch statement in non doc ready
 
 
-// value 
-const dogCategory = $('input[value=""]');
+// variable  of dog name
 
-switch (dogCategory) {
-    case 1:
-        console.log("Your dog is a Pupperino!");
-        break;
-    case 2:
-        console.log("Your dog is a Bean!");
-        break;
-    case 3:
-        console.log("Your dog is a Puppo!");
-        break;
-    case 4:
-        console.log("Your dog is a Pupper!");
-        break;
-    case 5:
-        console.log("Your dog is a Woofer!");
-        break;
-    case 6:
-        console.log("Your dog is a Boofer!");
-        break;
-    case 7:
-        console.log("Your dog is a Big Boofer!");
-        break;
-    default:
-        console.log("Ruh Roh! Try Again!");
+function sortDog() {
+    const dogName = $('.dogName')
+    console.log('the dog name', dogName);
+    //variable for dog girth
+    const dogCategory = $('select.dogSorter').val();
+    console.log('the dog category', dogCategory);
+    console.log(typeof dogCategory);
+    switch (dogCategory) {
+        case '1':
+            console.log("Your dog is a Pupperino!");
+
+            break;
+        case '2':
+            console.log("Your dog is a Bean!");
+            break;
+        case '3':
+            console.log("Your dog is a Puppo!");
+            break;
+        case '4':
+            console.log("Your dog is a Pupper!");
+            break;
+        case '5':
+            console.log("Your dog is a Woofer!");
+            break;
+        case '6':
+            console.log("Your dog is a Boofer!");
+            break;
+        case '7':
+            console.log("Your dog is a Big Boofer!");
+            break;
+        default:
+            console.log("Ruh Roh! Try Again!");
+    }
+
 }
 
 
 $(function () {
-    $('form').on('submit', function () {
+    $('form').on('submit', function(event) {
+        event.preventDefault();
+        sortDog();
         console.log('woof woof!')
+
+
     });
 });
