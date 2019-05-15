8274ee5cf66e02e6d877fb7666facb6f64375d49
Lobst3rG3off <erip.pasei@gmail.com>
2019-05-15 18:00:09 -0400

    almost done js, just need to fix input

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
 // variable  of dog name
 
 function sortDog() {
-    const dogName = $('.dogName')
+    const dogName = $('.dogName').value();
     console.log('the dog name', dogName);
     //variable for dog girth
     const dogCategory = $('select.dogSorter').val();
@@ -20,28 +20,36 @@ function sortDog() {
     switch (dogCategory) {
         case '1':
             console.log("Your dog is a Pupperino!");
-
+            $('.landingPad').empty()
+$('.landingPad').append('<p>Your dog is a Pupperino!</p>')
             break;
         case '2':
             console.log("Your dog is a Bean!");
+            $('.landingPad').append('<p>Your dog is a Pupperino!</p>')
             break;
         case '3':
             console.log("Your dog is a Puppo!");
+            $('.landingPad').append('<p>Your dog is a Puppo!</p>')
             break;
         case '4':
             console.log("Your dog is a Pupper!");
+            $('.landingPad').append('<p>Your dog is a  Pupper!</p>')
             break;
         case '5':
             console.log("Your dog is a Woofer!");
+            $('.landingPad').append('<p>Your dog is a Woofer!</p>')
             break;
         case '6':
             console.log("Your dog is a Boofer!");
+            $('.landingPad').append('<p>Your dog is a Boofer!</p>')
             break;
         case '7':
             console.log("Your dog is a Big Boofer!");
+            $('.landingPad').append('<p>Your dog is a Big Boofer!</p>')
             break;
         default:
             console.log("Ruh Roh! Try Again!");
+            $('.landingPad').append('<p>Ruh Roh! Try Again!</p>')
     }
 
 }
