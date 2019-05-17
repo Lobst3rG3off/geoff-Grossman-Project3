a854975145becb61570eb7ad417414654f00a84a
Lobst3rG3off <erip.pasei@gmail.com>
2019-05-17 17:00:17 -0400

    beer oclock

--- app.js
+++ app.js
@@ -15,42 +15,42 @@ function sortDog() {
         case '1':
            
             $('.landingPad').empty()
-$('.landingPad').append(`<h2>${dogName.val()} is a Pupperino!</h2>`)
+$('.landingPad').append(`<h2 class="dogAnswer">${dogName.val()} is a Pupperino!</h2>`)
             break;
         case '2':
             
             $('.landingPad').empty()
-            $('.landingPad').append(`<h2>${dogName.val()} is a Bean!</h2>`)
+            $('.landingPad').append(`<h2 class="dogAnswer">${dogName.val()} is a Bean!</h2>`)
             break;
         case '3':
             console.log("Your dog is a Puppo!");
             $('.landingPad').empty()
-            $('.landingPad').append(`<h2>${dogName.val()} is a Puppo!</h2>`)
+            $('.landingPad').append(`<h2 class="dogAnswer">${dogName.val()} is a Puppo!</h2>`)
             break;
         case '4':
             console.log("Your dog is a Pupper!");
             $('.landingPad').empty()
-            $('.landingPad').append(`<h2>${dogName.val()} is a Pupper!</h2>`)
+            $('.landingPad').append(`<h2 class="dogAnswer">${dogName.val()} is a Pupper!</h2>`)
             break;
         case '5':
             console.log("Your dog is a Woofer!");
             $('.landingPad').empty()
-            $('.landingPad').append(`<h2>${dogName.val()} is a Woofer!</h2>`)
+            $('.landingPad').append(`<h2 class="dogAnswer">${dogName.val()} is a Woofer!</h2>`)
             break;
         case '6':
             console.log("Your dog is a Boofer!");
             $('.landingPad').empty()
-            $('.landingPad').append(`<h2>${dogName.val()} is a  Boofer!</h2>`)
+            $('.landingPad').append(`<h2 class="dogAnswer">${dogName.val()} is a  Boofer!</h2>`)
             break;
         case '7':
             console.log("Your dog is a Big Boofer!");
             $('.landingPad').empty()
-            $('.landingPad').append(`<h2>${dogName.val()} is a Big Boofer!</h2>`)
+            $('.landingPad').append(`<h2 class="dogAnswer">${dogName.val()} is a Big Boofer!</h2>`)
             break;
         default:
             console.log("Ruh Roh! Try Again!");
             $('.landingPad').empty()
-            $('.landingPad').append('<h2>Ruh Roh! Try Again!</h2>')
+            $('.landingPad').append('<h2class="dogAnswer">Ruh Roh! Try Again!</h2>')
     }
 
 }
